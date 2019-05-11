dd743db2c81a053c74bd8f73ba84107cbf435535
Wes Todd <wes@wesleytodd.com>
2019-05-11 13:21:09 -0700

    fix bug with passing true to pino-http

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ module.exports = function createApp (main = () => {}, defaultOptions = {}) {
     parseJson: {},
     parseCookies: false,
     helmet: false,
-    pino: true
+    pino: {}
   }, defaultOptions)
 
   // Bind run method to these options
